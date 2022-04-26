095f689e418ff79bbe1a25e0996f1b44ad07044f
Timothy <104257662+Timothy156@users.noreply.github.com>
2022-04-27 00:05:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import os
+import time
 import telebot
 
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
+TOKEN = "your bot token here"
+bot = telebot.TeleBot(token=5353128873:AAGH4ZaQ9HJaooPaLaN95OT3dVNNCSBOfz8)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-  bot.send_message(message.chat.id, "Hello!")
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
 
+@bot.message_handler(commands=['start']) # welcome message handler
+def send_welcome(message):
+    bot.reply_to(message, '(placeholder text)')
 
+@bot.message_handler(commands=['help']) # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-@bot.message_handler(commands=['claim'])
-def claim(message):
-  bot.send_message(message.chat.id, "Please Enter a CLAIM KEY!")
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@': # in case it's just the '@', skip
+        pass
+    else:
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        bot.reply_to(message, insta_link)
 
-  
-@bot.message_handler(commands=[os.getenv('mKey')])
-def link(message):
-  bot.send_message(message.chat.id, os.getenv('mClaim'))
-
-  
-bot.polling()
+while True:
+    try:
+        bot.polling(none_stop=True)
+        # ConnectionError and ReadTimeout because of possible timout of the requests library
+        # maybe there are others, therefore Exception
+    except Exception:
+        time.sleep(15)
